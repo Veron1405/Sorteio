501493fbb844ac578b589bbe871e94c3b41828fb
Veron1405 <133662401+Veron1405@users.noreply.github.com>
2023-08-07 15:14:32 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,12 @@
+console.log()(Math.random());
+
+console.log(Math.random() * 20);
+
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
+
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
